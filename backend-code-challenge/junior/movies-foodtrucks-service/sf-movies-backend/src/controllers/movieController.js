c28fab5843149b55411514cd5a9fd5a365d2b1ff
Stephen <stevobialor@gmail.com~>
2024-04-05 19:20:03 +0100

    Add swagger documentation endpoint

---
+++ backend-code-challenge/junior/movies-foodtrucks-service/sf-movies-backend/src/controllers/movieController.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import catchAsync from '../errors/catchAsync.js';
+
+const getMovies = catchAsync(async (req, res, next) => {
+  const response = await axios.get(process.env.MOVIES_URL);
+
+  const { title } = req.query;
+
+  if (!response)
+    return res
+      .status(500)
+      .json({ status: 'error', error: 'data source unavailable' });
+
+  let { data } = response;
+
+  if (title) {
+    data = data.filter((movie) =>
+      movie.title.toLowerCase().includes(title.toLowerCase()),
+    );
+  }
+
+  if (data.length === 0) {
+    return res
+      .status(404)
+      .json({ status: 'error', error: 'no data available' });
+  }
+
+  data = data.map((movie) => ({
+    title: movie.title,
+    locations: movie.locations,
+    release_year: movie.release_year,
+  }));
+
+  return res.status(200).json({ status: 'success', data });
+});
+
+export default getMovies;
---
+++ backend-code-challenge/junior/movies-foodtrucks-service/sf-movies-backend/tests/E2E/getMovies.test.js
@@ -0,0 +1,127 @@
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
+import request from 'supertest';
+import axios from 'axios';
+import { config } from 'dotenv';
+import app from '../../src/app';
+
+const envPath = new URL('../../.env', import.meta.url).pathname;
+config({ path: envPath });
+
+vi.mock('axios');
+
+describe('/GET movies', () => {
+  it('Returns expected data when third party API is available', async () => {
+    const mockData = {
+      data: [
+        {
+          title: 'mockTitle',
+          locations: 'mockLocation',
+          release_year: 2000,
+          production_company: 'mockCompany',
+          distribution: 'mockDistribution',
+          director: 'mockDirector',
+        },
+      ],
+    };
+
+    axios.get.mockResolvedValue(mockData);
+
+    const response = await request(app).get('/api/v1/movies');
+    console.log(response.body.data);
+    console.log(mockData.data);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.status).toBe('success');
+    expect(response.body.data[0].title).toBe(mockData.data[0].title);
+    expect(response.body.data[0].locations).toBe(mockData.data[0].locations);
+    expect(response.body.data[0].release_year).toBe(
+      mockData.data[0].release_year,
+    );
+  });
+
+  it('Returns expected data with querry parameter when third party API is available', async () => {
+    const mockData = {
+      data: [
+        {
+          title: 'love',
+          locations: 'mockLocation1',
+          release_year: 2000,
+          production_company: 'mockCompany',
+          distribution: 'mockDistribution',
+          director: 'mockDirector',
+        },
+        {
+          title: 'charge',
+          locations: 'mockLocation2',
+          release_year: 2000,
+          production_company: 'mockCompany',
+          distribution: 'mockDistribution',
+          director: 'mockDirector',
+        },
+        {
+          title: 'love',
+          locations: 'mockLocation3',
+          release_year: 2000,
+          production_company: 'mockCompany',
+          distribution: 'mockDistribution',
+          director: 'mockDirector',
+        },
+        {
+          title: 'charge',
+          locations: 'mockLocation4',
+          release_year: 2000,
+          production_company: 'mockCompany',
+          distribution: 'mockDistribution',
+          director: 'mockDirector',
+        },
+        {
+          title: 'charge',
+          locations: 'mockLocation5',
+          release_year: 2000,
+          production_company: 'mockCompany',
+          distribution: 'mockDistribution',
+          director: 'mockDirector',
+        },
+      ],
+    };
+
+    axios.get.mockResolvedValue(mockData);
+
+    const response = await request(app).get('/api/v1/movies?title=love');
+    console.log(response.body.data);
+    console.log(mockData.data);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.status).toBe('success');
+    expect(response.body.data.length).toBe(2);
+    expect(response.body.data[0].locations).toBe(mockData.data[0].locations);
+    expect(response.body.data[1].locations).toBe(mockData.data[2].locations);
+    expect(response.body.data[0].release_year).toBe(
+      mockData.data[0].release_year,
+    );
+  });
+
+  it('Returns proper erro message when no data is available', async () => {
+    const mockData = { data: [] };
+
+    axios.get.mockResolvedValue(mockData);
+
+    const response = await request(app).get('/api/v1/movies');
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.status).toBe('error');
+    expect(response.body.error).toBe('no data available');
+  });
+
+  it('Returns error when API is unavailable', async () => {
+    const mockData = null;
+
+    axios.get.mockResolvedValue(mockData);
+
+    const response = await request(app).get('/api/v1/movies');
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body.status).toBe('error');
+    expect(response.body.error).toBe('data source unavailable');
+  });
+});
