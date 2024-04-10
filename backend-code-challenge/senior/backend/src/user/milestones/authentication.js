b96d6353e9e9d4d980a87112e01d8f04892bc8b4
Stephen <stevobialor@gmail.com~>
2024-04-10 19:25:50 +0100

    Add validation for slug

--- backend-code-challenge/senior/backend/src/user/milestones/authentication.js
+++ backend-code-challenge/senior/backend/src/user/milestones/authentication.js
@@ -1,16 +1,18 @@
 'use strict';
 
-// const {
-//   Errors: { BadRequestError },
-// } = require('finale-rest');
+const {
+  Errors: { BadRequestError },
+} = require('finale-rest');
 
-// const {
-//   API: {
-//     HEADERS: { X_SLUG },
-//     SLUGS: { MYSELF },
-//   },
-// } = require('../../config');
+const {
+  API: {
+    HEADERS: { X_SLUG },
+    SLUGS: { MYSELF },
+  },
+} = require('../../config');
 
 module.exports = (req, res, context) => {
+  const slug = req.headers[X_SLUG];
+  if (slug !== MYSELF) return context(new BadRequestError());
   return context.continue;
 };
