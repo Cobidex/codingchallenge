86efb48bf02234385cda14296146dea601e8a92f
Stephen <stevobialor@gmail.com~>
2024-04-10 19:17:16 +0100

    Add validation for api-key

--- backend-code-challenge/senior/backend/src/user/milestones/authorization.js
+++ backend-code-challenge/senior/backend/src/user/milestones/authorization.js
@@ -1,16 +1,18 @@
 'use strict';
 
-// const {
-//   Errors: { ForbiddenError },
-// } = require('finale-rest');
+const {
+  Errors: { ForbiddenError },
+} = require('finale-rest');
 
-// const {
-//   API: {
-//     KEY,
-//     HEADERS: { X_API_KEY },
-//   },
-// } = require('../../config');
+const {
+  API: {
+    KEY,
+    HEADERS: { X_API_KEY },
+  },
+} = require('../../config');
 
 module.exports = (req, res, context) => {
+  const apiKey = req.headers[X_API_KEY];
+  if (apiKey !== KEY) return context(new ForbiddenError());
   return context.continue;
 };
