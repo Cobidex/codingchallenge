a92b279fb17e8acc48339bc26fa4e79fa6e5cdae
Stephen <stevobialor@gmail.com~>
2024-04-10 19:44:50 +0100

    Populate favourites list

--- backend-code-challenge/senior/backend/src/user/milestones/presentation.js
+++ backend-code-challenge/senior/backend/src/user/milestones/presentation.js
@@ -1,14 +1,26 @@
 'use strict';
 
-// const axios = require('axios');
+const axios = require('axios');
 
-// const {
-//   STAR_WARS_API: {
-//     BASE_URL,
-//     ENDPOINTS: { PEOPLE },
-//   },
-// } = require('../../config');
+const {
+  STAR_WARS_API: {
+    BASE_URL,
+    ENDPOINTS: { PEOPLE },
+  },
+} = require('../../config');
 
 module.exports = async (req, res, context) => {
+  const { favourites } = req.body;
+  const asyncFns = favourites.map(
+    (id) =>
+      new Promise(() => {
+        const uri = `${BASE_URL}/${PEOPLE}/${id}/`;
+        return axios.get(uri);
+      }),
+  );
+
+  const favItems = await Promise.all(asyncFns);
+  res.favouritesDetails = favItems;
+
   return context.continue;
 };
